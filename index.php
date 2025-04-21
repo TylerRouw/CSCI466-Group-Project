001505d6bcb178ac6720bff0e3ec9e73e7b27ac5
TylerRouw <tyler.rouw@icloud.com>
2025-04-21 16:11:10 -0500

    added, starter index, login and product pages.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>This is the home page</title>
+</head>
+<body>
+  <h1>Welcome to the site!</h1>
+  <a href="login.php">Login</a><br/>
+  <a href="product.php">Products</a><br/>
+  <a href="orders.php">Orders</a><br/>
+  <a href="cart.php">Cart</a><br/>
+</body>
+</html>
+
